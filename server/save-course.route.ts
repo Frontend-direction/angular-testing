6a528ea425d5f42dfeeb63180414d894e3f014aa
Volodymyr Pankiv <vpanki@softserveinc.com>
2024-08-28 11:12:56 +0300

    update setup
    Signed-off-by: Volodymyr Pankiv <vpanki@softserveinc.com>

--- server/save-course.route.ts
+++ server/save-course.route.ts
@@ -1,11 +1,8 @@
-
-import {Request, Response} from 'express';
-import {findCourseById} from './db-data';
-
+import { Request, Response } from "express";
+import { findCourseById } from "./db-data";
 
 export function saveCourse(req: Request, res: Response) {
-
-  const id = req.params["id"],
+  const id: any = req.params["id"],
     changes = req.body;
 
   console.log("Saving course", id, JSON.stringify(changes));
@@ -15,6 +12,4 @@ export function saveCourse(req: Request, res: Response) {
   course.titles = changes.titles;
 
   res.status(200).json(course);
-
 }
-
